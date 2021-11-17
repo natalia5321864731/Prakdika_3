776498ea6693c619bd828eb9935a365aaf0f1bb5
natalia5321864731 <92432369+natalia5321864731@users.noreply.github.com>
2021-11-17 13:57:30 +0300

    Add files via upload

---
+++ pr_3/pr_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, ребята");
+            Console.ReadKey();
+        }
+    }
+}
